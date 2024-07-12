b5ad34fb29c54c71b5f375e30eb3968dc23df073
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-12 21:28:35 +0530

    Initial Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+};
+
+export default Home;
